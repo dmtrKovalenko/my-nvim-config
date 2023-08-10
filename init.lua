77b54d31939542fbf3bcc2a8f79b540bb18ec744
Dmitriy <dmtr.kovalenko@outlook.com>
2023-08-10 22:51:01 +0200

    Change theme for the config

--- init.lua
+++ init.lua
@@ -97,7 +97,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'palenight',
         component_separators = '|',
         section_separators = '',
       },
